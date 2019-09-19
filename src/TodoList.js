6eb33a2506c80559deb4a22ab6c45c69423ac912
Yuri <ymoraes5948@gmail.com>
2019-09-19 16:56:25 -0300

    aula 6

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as TodosActions from './store/actions/todos';
 
-const TodoList = ({todos, dispatch}) => (
+const TodoList = ({todos, addTodo, markAsCompleted}) => (
   <View>
     {todos.map(todo => (
       <Text
-        onPress={() => dispatch({type: 'MARK_AS_COMPLETED', id: todo.id})}
+        onPress={markAsCompleted(todo.id)}
         style={{textDecorationLine: todo.completed ? 'line-through' : 'none'}}
         key={todo.id}>
         {todo.text}
       </Text>
     ))}
-    <Button
-      title={'Adicionar todo'}
-      onPress={() => dispatch({type: 'ADD_TODO', text: 'Novo todo'})}
-    />
+    <Button title={'Adicionar todo'} onPress={addTodo} />
   </View>
 );
 
@@ -23,4 +22,10 @@ const mapStateToProps = state => ({
   todos: state,
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(TodosActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TodoList);
